41b134bf63840d98dc5bca7f69b00f8370d78ccf
Gabriel Samaniego <selfdev.gs@gmail.com>
2020-01-08 10:43:41 -0800

    Fixes player walking animation

--- client/src/player/Player.js
+++ client/src/player/Player.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useLayoutEffect, useRef} from "react";
 import character from "../assets/Characters/character.png";
 import {connect} from "react-redux";
 import {store} from "../store";
@@ -6,50 +6,41 @@ import handleMovement from "../player/movement";
 import "../index.css";
 
 const Player = props => {
-  //   const [walking, setWalking] = useState(false);
-  //   console.log(walking);
+  // Handles player walking animation
+  const playerRef = useRef();
 
-  // useEffect(() => {
-  //   setWalking(true);
-  //   setTimeout(() => {
-  //     setWalking(false);
-  //   }, 2000);
-  // }, []);
+  useLayoutEffect(() => {
+    playerRef.current.classList.add("walk");
 
-  //   const handleWalk = () => {
-  //     setWalking(true);
-  //     setTimeout(() => {
-  //       setWalking(false);
-  //     }, 2000);
-  //   };
+    return () => {
+      setTimeout(() => {
+        playerRef.current.classList.remove("walk");
+      }, 400);
+    };
+  }, [props.position]);
+
+  useLayoutEffect(() => {
+    // prevents player from marching in place at game start
+    playerRef.current.classList.remove("walk");
+  }, []);
 
   return (
     <div
-      //   onKeyDown={() => setWalking(!walking)}
-      //   onKeyDown={() => handleWalk()}
       style={{
         position: "absolute",
         top: props.position[1],
         left: props.position[0],
-        // backgroundImage: `url('${character}')`,
-        // backgroundPosition: props.spriteLocation,
         width: "40px",
         height: "60px",
         overflow: "hidden",
-        // border: "1px solid red",
         marginLeft: "10px"
       }}
     >
       <img
+        ref={playerRef}
         className={`character_sprite pixelate ${props.spriteLocation}`}
         src={character}
         alt="character"
-        style={
-          {
-            //   position: "absolute",
-            //   bottom: `${props.spriteLocation}`
-          }
-        }
       />
     </div>
   );
