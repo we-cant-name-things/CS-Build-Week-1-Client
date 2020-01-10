3a9f1e0d2ff87dcf567783ca69320b8a527a3f03
Josh Diamond <diamondroad91@gmail.com>
2020-01-10 07:41:44 -0700

    required fields for login/create

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../App.css'
 import { Link } from 'react-router-dom'
+import Axios from "axios";
 
 export default function Menu({ history }) {
 
@@ -17,12 +18,26 @@ export default function Menu({ history }) {
       window.location.reload()
   }
 
+  const resetGame = e => {
+      e.preventDefault()
+      Axios
+        .put('https://we-cant-name-things.herokuapp.com/api/move', {
+            "email": "seanwu20@gmail.com",
+            "food":12,
+            "water":32,
+            "new_city":"Miami"
+        })
+        .then(res => console.log('resetRES', res))
+        .catch(err => console.log(err))
+  }
+
   return (
     <div className="menu">
       <h3>Menu</h3>
       <div className="stat column">
         <button onClick={logout} className='logout_btn'>Log Out</button>
         <button onClick={map} className='logout_btn' style={{marginTop: '15px'}}>View Map</button>
+        <button onClick={resetGame} className='logout_btn' style={{marginTop: '15px'}}>Reset</button>
       </div>
     </div>
   );
